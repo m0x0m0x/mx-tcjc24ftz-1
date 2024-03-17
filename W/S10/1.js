8cf3ada9a93967ddfc729d7438390dcfae27421b
m0x0m0x <mx0mx@tuta.io>
2024-03-18 04:31:59 +0530

    c

--- W/S10/1.js
+++ W/S10/1.js
@@ -3,7 +3,7 @@
 Section 10 : 1st JS test
 */
 //=== Import zone
-import { mainHead, subHead, paraText } from "../../utils/text";
+import { mainHead, subHead, paraText } from "../../utils/text.js";
 //
 mainHead("Section 10 : 1js Test");
 console.log("Hey");
