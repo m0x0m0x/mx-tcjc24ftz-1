a322ba02e19da4d7420a4fa7a993872200324a84
m0x0m0x <mx0mx@tuta.io>
2024-03-18 17:25:52 +0530

    addd

---
+++ W/S10/129.js
@@ -0,0 +1,11 @@
+"use strict";
+/* 
+129: Default Parameters 
+*/
+// === import zone ===
+import { mainHead, subHead, paraText } from "../../utils/text.js";
+// ======
+mainHead("129: Default Parameters ");
+
+subHead("Section start - Using Airport Theme");
+const creatBooking = function (flightNum, numPass, price)
---
+++ testing/t1.js
@@ -0,0 +1,10 @@
+/*
+T1 
+Testing random function which you remembered while doing  W/S10/129.js
+*/
+// === Import Zone ===
+import { mainHead, subHead, paraText } from "../utils/text.js";
+//
+
+mainHead("T1 - Test1.js");
+console.log("asds");
