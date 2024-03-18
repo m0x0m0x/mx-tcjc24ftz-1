bbf44182af96d42f0523f7e004c927440df294a9
m0x0m0x <mx0mx@tuta.io>
2024-03-18 17:45:09 +0530

    cc

--- W/S10/129.js
+++ W/S10/129.js
@@ -10,6 +10,7 @@ mainHead("129: Default Parameters ");
 // Starting with making booking function
 subHead("Section start - Using Airport Theme");
 
+const bookingS = [];
 const creatBooking = function (flightNum, numPass, price) {
   const booking = {
     flightNum,
@@ -17,4 +18,7 @@ const creatBooking = function (flightNum, numPass, price) {
     price,
   };
   console.log(booking);
+  bookingS.push(booking);
 };
+
+creatBooking("Jiuo", 23, 900);
