8f40c03be86f71322bfdc4db5db6eb9f0df0c8c0
m0x0m0x <mx0mx@tuta.io>
2024-03-18 17:12:03 +0530

    add

--- testing/testing.js
+++ testing/testing.js
@@ -6,3 +6,6 @@ import { mainHead, subHead, paraText } from "../utils/text.js";
 ///
 
 mainHead("main head test");
+subHead(
+  "All tests will be done here instead of in other packages, and then you can push all this to ur repor also"
+);
