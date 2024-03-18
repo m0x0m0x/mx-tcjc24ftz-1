92e92ee490ae94ad2a1cf37dd93009103175ac1d
m0x0m0x <mx0mx@tuta.io>
2024-03-18 20:54:28 +0530

    132

---
+++ W/S10/132.js
@@ -0,0 +1,8 @@
+"use strict";
+/* 
+132: Functions Accepting Callback Functions
+*/
+// === import zone ===
+import { mainHead, subHead, paraText } from "../../utils/text.js";
+// ======
+mainHead("132: Functions Accepting Callback Functions");
