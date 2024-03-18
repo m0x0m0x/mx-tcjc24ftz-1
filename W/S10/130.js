a342861562b4e383777a8e998fd7469f2fc6e30e
m0x0m0x <mx0mx@tuta.io>
2024-03-18 18:07:54 +0530

    add

--- W/S10/130.js
+++ W/S10/130.js
@@ -6,3 +6,11 @@
 import { mainHead, subHead, paraText } from "../../utils/text.js";
 // ======
 mainHead("130: How Passing Arguments Works: Value vs Reference");
+
+// Learning about primitives and objects as a base
+subHead("Primitives and Objects in terms of Functions");
+const flight = "LH234";
+const nina = {
+  name: "Mistress Nina",
+  pass: "6969",
+};
