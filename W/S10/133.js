cf15614dfd56c32e7ec0a1c16d93b147d826e806
m0x0m0x <mx0mx@tuta.io>
2024-03-18 23:13:33 +0530

    :)

--- W/S10/133.js
+++ W/S10/133.js
@@ -1,8 +1,20 @@
 "use strict";
 /* 
 133: Functions Returning Functions
+- Function that returns another function
 */
 // === import zone ===
 import { mainHead, subHead, paraText } from "../../utils/text.js";
 // ======
 mainHead("133: Functions Returning Functions");
+
+subHead("Testin return function");
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeter = greet("hey");
+greeter("Nina");
+greeter("Nbina");
