7986a92ee3a61c6742a0c826c4446690034c83fc
m0x0m0x <mx0mx@tuta.io>
2024-03-19 03:46:52 +0530

    add

---
+++ W/S10/134.js
@@ -0,0 +1,8 @@
+"use strict";
+/* 
+134: The Call and Apply Methods 
+*/
+// === import zone ===
+import { mainHead, subHead, paraText } from "../../utils/text.js";
+// ======
+mainHead("134: The Call and Apply Methods ");
