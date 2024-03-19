5fd29156654bbfeb09bcd608d372c36ca50c4a73
m0x0m0x <mx0mx@tuta.io>
2024-03-19 22:56:28 +0530

    135 Bind

---
+++ W/S10/135.js
@@ -0,0 +1,10 @@
+"use strict";
+/* 
+135: The Bing Method
+- Continunation from 134.js , when learning about 
+this alternatives , previous was, book.call and book.apply
+*/
+// === import zone ===
+import { mainHead, subHead, paraText } from "../../utils/text.js";
+// ======
+mainHead("135: The Bind Method");
