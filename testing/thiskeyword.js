3d49b7525a326450abfc319a5fba3ba7294afcf2
m0x0m0x <mx0mx@tuta.io>
2024-03-19 20:31:09 +0530

    add

---
+++ testing/thiskeyword.js
@@ -0,0 +1,8 @@
+/*
+Revision of this keyword which is 
+ch98- this Keyword , You came here when going through 134:call and apply methods 
+*/
+// === import zone
+// Note this will bring in all the functions
+import {} from "../utils/text.js";
+//
