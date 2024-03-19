73cfb62ad657681f2459bcf343c13acff3b81797
m0x0m0x <mx0mx@tuta.io>
2024-03-19 22:46:06 +0530

    ✈️

--- W/S10/134.js
+++ W/S10/134.js
@@ -10,12 +10,42 @@ mainHead("134: The Call and Apply Methods ");
 
 subHead("Testing methods");
 const butyAir = {
-  airline: "Buuty",
+  airline: "BuutySniffAir",
   iataCode: "BX",
   bookings: [],
   book(fltNum, passN) {
     console.log(
-      `${passN} booked seat on ${this.airline} flight ${this.iataCode}${this.fltNum}`
+      `${passN} booked seat on ${this.airline} flight ${this.iataCode}${fltNum}`
     );
+    this.bookings.push({ flight: `${this.iataCode}${fltNum}`, passN });
   },
 };
+paraText("Printing object with pushed elemens");
+console.log(butyAir);
+
+paraText("Print Object with function");
+butyAir.book("69", "Jina");
+butyAir.book("69", "Mona");
+
+paraText("Another Object and testing methods");
+const pantyAir = {
+  name: "PussyWings",
+  iataCode: "PS",
+  bookings: [],
+};
+
+// Create a new reusable function
+const book = butyAir.book;
+console.log(`
+Type of object ${typeof book}
+`);
+// This will not work since this is undefined
+// book(23, "ika");
+
+subHead("Using call and apply methods");
+
+// Using the call method
+book.call(pantyAir, 99, "gina");
+console.log(pantyAir);
+book.call(butyAir, 57, "Lona");
+console.log(butyAir);
